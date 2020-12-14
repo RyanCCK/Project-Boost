fc40cff02e4e97a6f7d4f755a2159daaddc02bed
RyanCCK <RyanCK.Dev@gmail.com>
2020-12-13 21:28:51 -0700

    Added Control Bindings
    Messages are now printed in the console when the player presses the keys that will correspond to thrust, left rotation, and right rotation for the player character.

---
+++ Project Boost/Assets/RocketMan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Thrusting!");
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            print("Rotating Right!");
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            print("Rotating Left!");
+        }
+    }
+}
