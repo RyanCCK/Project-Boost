c85e5e60a1c47b0c7d79bf70bff7f376f12c467c
RyanCCK <RyanCK.Dev@gmail.com>
2020-12-24 13:27:08 -0700

    Improved audio, refactored and commented code
    There are now audio effects for dying and progressing to the next level. There is now a slight (intentional) delay after death or success, to provide time for effects to occur. Code has been refactored to improve readability, and more comments have been added.

--- Project Boost/Assets/RocketMan.cs
+++ Project Boost/Assets/RocketMan.cs
@@ -9,8 +9,17 @@ public class RocketMan : MonoBehaviour
     Rigidbody rigidBody;
     AudioSource audioSource;
 
+    enum State {Alive, Dying, Progressing};
+    State state = State.Alive;
+
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 4f;
+    [SerializeField] AudioClip thrustSound;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip successSound;
+
+    float nextSceneLoadTime = 1.5f;
+    float deathTime = 1.5f;
 
     // Start is called before the first frame update.
     void Start()
@@ -22,30 +31,39 @@ public class RocketMan : MonoBehaviour
     // Update is called once per frame.
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            HandleThrustInput();
+            HandleRotationInput();
+        }
     }
 
     // Apply thrust to rocket and play thrust sound effect when space key is pressed.
-    private void Thrust()
+    private void HandleThrustInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ApplyThrust();
         }
         else audioSource.Stop();
     }
 
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(thrustSound);
+        }
+    }
+
     // Rotate rocket clockwise or counterclockwise when right or left arrow keys are pressed.
-    private void Rotate()
+    private void HandleRotationInput()
     {
         // Freeze physics control of rotation
         rigidBody.freezeRotation = true;
 
+        // For framerate independence.
         float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.RightArrow))
@@ -61,20 +79,55 @@ public class RocketMan : MonoBehaviour
         rigidBody.freezeRotation = false;
     }
 
+    // Handle collisions.
     private void OnCollisionEnter(Collision collision)
     {
+        // Ignore collisions when not alive.
+        if (state != State.Alive) {return;} 
+
+        // Handle collisions based on tag of game object the player collides with.
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 break;
 
             case "Finish":
-                SceneManager.LoadScene(1);
+                ProgressPlayer();
                 break;
 
             default:
-                SceneManager.LoadScene(0);
+                KillPlayer();
                 break;
         }
+    }
+
+    // Move to the next stage of the game when the player completes the objective in the current stage.
+    private void ProgressPlayer()
+    {
+        state = State.Progressing;
+        audioSource.Stop();
+        audioSource.PlayOneShot(successSound);
+        Invoke("LoadNextScene", nextSceneLoadTime);
+    }
+
+    // Handle all events that occur when the player dies.
+    private void KillPlayer()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathSound);
+        Invoke("LoadFirstScene", deathTime);
+    }
+
+    // Load the next scene.
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Load the first scene.
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
